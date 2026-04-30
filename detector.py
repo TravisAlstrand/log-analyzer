a83b53f7f284bf7c24a0a47eb42239a0c40cb7a2
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2026-04-30 12:32:42 -0700

    suspicious IPs function implemented

--- detector.py
+++ detector.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+
 def detect_failed_login_attempts(logs: list):
     results = []
 
@@ -9,3 +12,20 @@ def detect_failed_login_attempts(logs: list):
                 f"{log["timestamp"]} - Failed login for {user} from {ip}")
 
     return results
+
+
+def detect_suspicious_ips(logs: list, threshold: int = 3):
+    ip_count = defaultdict(int)
+
+    for log in logs:
+        ip = log["details"].get("ip")
+        if ip:
+            ip_count[ip] += 1
+
+    suspicious_ips = []
+
+    for ip, count in ip_count.items():
+        if count >= threshold:
+            suspicious_ips.append(f"{ip} triggered {count} events")
+
+    return suspicious_ips
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import typer
 from parser import parse_log
-from detector import detect_failed_login_attempts
+from detector import detect_failed_login_attempts, detect_suspicious_ips
 
 app = typer.Typer()
 
@@ -9,17 +9,33 @@ app = typer.Typer()
 def analyze(
     file: str,
     failed_logins: bool = False,
-    suspicious_ip: bool = False
+    suspicious_ips: bool = False
 ):
     parsed_logs = parse_log(file)
 
-    if (failed_logins):
+    if failed_logins:
         results = detect_failed_login_attempts(parsed_logs)
         print(f"--{len(results)} FAILED LOGIN ATTEMPTS DETECTED--")
         for result in results:
             print(result)
+    elif suspicious_ips:
+        results = []
+        user_input = input("What is the threshold? (3 is default) ")
+
+        if user_input.strip() == "":
+            results = detect_suspicious_ips(parsed_logs)
+        else:
+            try:
+                threshold = int(user_input)
+                results = detect_suspicious_ips(parsed_logs, threshold)
+            except ValueError:
+                print("Not a valid threshold input. Using default of 3.")
+                results = detect_suspicious_ips(parsed_logs)
+
+        for result in results:
+            print(result)
     else:
-        print(f"--ALL LOGS--")
+        print("--ALL LOGS--")
         for log in parsed_logs:
             for key, value in log.items():
                 if key == "details":
