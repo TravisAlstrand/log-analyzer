ae3f9797346aadec74a3aaaad5aef4726c0f4869
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2026-05-01 17:38:15 -0700

    stats returning time range

--- loganalyzer/reports/stats.py
+++ loganalyzer/reports/stats.py
@@ -1,12 +1,35 @@
 from loganalyzer.core.loader import load_logs
+from datetime import datetime
 
 
 def get_stats(file_path: str) -> list[str]:
-    stats = []
     parsed_logs = load_logs(file_path)
+    stats = compose_stats(parsed_logs)
+    return stats
 
+
+def compose_stats(logs: list[dict]) -> list[str]:
+    stats = []
     stats.append("--- LOG FILE STATISTICS ---\n")
     stats.append("1. File Summary")
-    stats.append(f"  - Total Events: {len(parsed_logs)}")
+    stats.append(f"  - Total Events: {len(logs)}")
+    timeframe = get_timeframe(logs)
+    stats.append("2. Time Range")
+    stats.append(f"  - {timeframe[0]} - {timeframe[1]}\n")
 
     return stats
+
+
+def get_timeframe(logs: list[dict]) -> list[datetime | None]:
+    earliest = None
+    latest = None
+
+    for log in logs:
+        timestamp = datetime.strptime(log["timestamp"], "%Y-%m-%d %H:%M:%S")
+        if earliest is None or timestamp < earliest:
+            earliest = timestamp
+
+        if latest is None or timestamp > latest:
+            latest = timestamp
+
+    return [earliest, latest]
