42e7c80a3c2a4f328c11bbe12191f555cde1f8a1
Travis Alstrand <92395688+TravisAlstrand@users.noreply.github.com>
2026-05-02 08:17:52 -0700

    event counts showing in stats

--- loganalyzer/reports/stats.py
+++ loganalyzer/reports/stats.py
@@ -1,5 +1,6 @@
 from loganalyzer.core.loader import load_logs
 from datetime import datetime
+from collections import defaultdict
 
 
 def get_stats(file_path: str) -> list[str]:
@@ -13,14 +14,18 @@ def compose_stats(logs: list[dict]) -> list[str]:
     stats.append("--- LOG FILE STATISTICS ---\n")
     stats.append("1. File Summary")
     stats.append(f"  - Total Events: {len(logs)}")
-    timeframe = get_timeframe(logs)
+    earliest, latest = get_timeframe(logs)
     stats.append("2. Time Range")
-    stats.append(f"  - {timeframe[0]} - {timeframe[1]}\n")
+    stats.append(f"  - {earliest} - {latest}\n")
+    stats.append("--- EVENT TYPE BREAKDOWN ---\n")
+    event_types = get_event_counts(logs)
+    for event in event_types:
+        stats.append(event)
 
     return stats
 
 
-def get_timeframe(logs: list[dict]) -> list[datetime | None]:
+def get_timeframe(logs: list[dict]) -> tuple[datetime | None, datetime | None]:
     earliest = None
     latest = None
 
@@ -32,4 +37,18 @@ def get_timeframe(logs: list[dict]) -> list[datetime | None]:
         if latest is None or timestamp > latest:
             latest = timestamp
 
-    return [earliest, latest]
+    return earliest, latest
+
+
+def get_event_counts(logs: list[dict]) -> list[str]:
+    results = []
+    event_count = defaultdict(int)
+
+    for log in logs:
+        event = log["event"]
+        event_count[event] += 1
+
+    for event, count in event_count.items():
+        results.append(f"  - {event}: {count}")
+
+    return results
